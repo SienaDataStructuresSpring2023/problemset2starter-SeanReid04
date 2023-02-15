482e135b7b09694ecf8e3f08e9b564b053147520
SeanReid04 <s06reid@siena.edu>
2023-02-15 09:07:03 -0500

    created instance variables

--- StockHolding.java
+++ StockHolding.java
@@ -6,11 +6,18 @@
  */
 public class StockHolding
 {
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
-        // symbol, name, numShares, price, numShares * price); 
-    // }
+    private char symbol;
+    private String name;
+    private int numShares;
+    private double price;
+    
+    
+    
+    @Override
+    public String toString()
+    {
+        //DO NOT EDIT THIS METHOD.
+        return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
+            symbol, name, numShares, price, numShares * price); 
+    }
 }

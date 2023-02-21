f420cb2edbf16dfcc80c027c0e880d5237eccff4
SeanReid04 <s06reid@siena.edu>
2023-02-21 18:08:20 -0500

    cleaned up starter and made ArrayList instance variable

--- Portfolio.java
+++ Portfolio.java
@@ -1,24 +1,29 @@
+import java.util.ArrayList;
 
 /**
  * Write a description of class Portfolio here.
  *
- * @author (your name)
+ * @author Sean Reid
  * @version (a version number or a date)
  */
 public class Portfolio
 {
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // StringBuffer sb = new StringBuffer();
-        // sb.append(String.format("%6s%25s%10s%15s%15s%n", 
-                // "Symbol", "Name", "Shares", "@Price", "Total"));
-        // sb.append("-----------------------------------------------------------------------\n");
+    private ArrayList<StockHolding> stocks;
+    
 
-        // for(StockHolding s : stocks){
-            // sb.append(s.toString());
-        // }
-        // return sb.toString();
-    // }
+    
+    @Override
+    public String toString()
+    {
+        //DO NOT EDIT THIS METHOD.
+        StringBuffer sb = new StringBuffer();
+        sb.append(String.format("%6s%25s%10s%15s%15s%n", 
+                 "Symbol", "Name", "Shares", "@Price", "Total"));
+                sb.append("-----------------------------------------------------------------------\n");
+
+        for(StockHolding s : stocks){
+            sb.append(s.toString());
+        }
+        return sb.toString();
+    }
 }

3818a6fc31a601aca123f8e057b0ffd5bdb9fe3e
SeanReid04 <s06reid@siena.edu>
2023-02-23 19:25:58 -0500

    api mistakes and professor help

--- Portfolio.java
+++ Portfolio.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 
 /**
- * Write a description of class Portfolio here.
+ * 
+ * This class creates a Portfolio object along with methods in order to manipulate the object in a desired manner.
  *
- * @author Sean Reid
- * @version Spring 2023
+ * @author Sean Reid.
+ * @version Spring 2023.
  */
 public class Portfolio
 {
@@ -15,7 +16,7 @@ public class Portfolio
     private double lifeP;
 
     /**
-     * Constructs and returns a Portfolio object initializing all the instance variables
+     * Constructs and returns a Portfolio object initializing all the instance variables.
      */
     public Portfolio(){
         stocks = new ArrayList<StockHolding>();
@@ -24,30 +25,30 @@ public class Portfolio
     }
 
     /**
-     * Returns the lifetime investment for this stock
+     * Returns the lifetime investment for this stock.
      * 
-     * @return lifeI the lifetime investment for this stock
+     * @return lifeI the lifetime investment for this stock.
      */
     public double getLifeI(){
         return lifeI;
     }
 
     /**
-     * Returns the lifetime payout for this stock
+     * Returns the lifetime payout for this stock.
      * 
-     * @return lifeP the lifetime payout for this stock
+     * @return lifeP the lifetime payout for this stock.
      */
     public double getLifeP(){
         return lifeP;
     }
 
     /**
-     * Returns the index of the ArrayList the matches the symbol passed in
+     * Returns the index of the ArrayList the matches the symbol passed in.
      * 
-     * @param symbol the symbol for the stock
-     * @return the index of the ArrayList the matches the symbol passed in or -1 if the symbol is not found
+     * @param symbol the symbol for the stock.
+     * @return the index of the ArrayList the matches the symbol passed in or -1 if the symbol is not found.
      */
-    private int getIndex(String symbol){
+    private int getIndex(String symbol){//while loop
         for (int i = 0; i < stocks.size(); i++){
             if (stocks.get(i).getSymbol().equalsIgnoreCase(symbol)){
                 return i;
@@ -57,13 +58,13 @@ public class Portfolio
     }
 
     /**
-     * Buys more shares of the stock if stock is already owned, otherwise the stock is bought and added to the ArrayList, also returns the cost of buying the stock
+     * Buys more shares of the stock if stock is already owned, otherwise the stock is bought and added to the ArrayList, also returns the cost of buying the stock.
      * 
-     * @param symbol the symbol for the stock
-     * @param name the name of the stock
-     * @param numShares the number shares owned for this stock
-     * @param price the price of the stock per share
-     * @return the cost of buying of the stock
+     * @param symbol the symbol for the stock.
+     * @param name the name of the stock.
+     * @param numShares the number shares owned for this stock.
+     * @param price the price of the stock per share.
+     * @return the cost of buying of the stock.
      */
     public double buyStock(String symbol, String name, int numShares, double price){
 
@@ -75,42 +76,40 @@ public class Portfolio
             stocks.add(st);
         }
 
-        
-        lifeI = numShares * price;
-        return lifeI;    
+        lifeI += numShares * price;
+        return numShares * price;    
     }
 
     /**
-     * Returns the profit made from selling shares if unable to sell shares then 0 is returned
+     * Returns the profit made from selling shares if unable to sell shares then 0 is returned.
      * 
-     * @param symbol the symbol for the stock
-     * @param numShares the number shares owned for this stock
-     * @return the amount of profit made from selling the shares
+     * @param symbol the symbol for the stock.
+     * @param numShares the number shares owned for this stock.
+     * @return the amount of profit made from selling the shares.
      */
     public double sellStock(String symbol, int numShares){
         double cost = 0;
-        
-            if (getIndex(symbol) != -1){
-                cost = stocks.get(getIndex(symbol)).sellShares(numShares);
-                if (stocks.get(getIndex(symbol)).getNumShares() == 0){
-                    stocks.remove(getIndex(symbol));
-                }
-                lifeP += cost;
-            }
 
-        
+        if (getIndex(symbol) != -1){
+            cost = stocks.get(getIndex(symbol)).sellShares(numShares);
+            if (stocks.get(getIndex(symbol)).getNumShares() == 0){
+                stocks.remove(getIndex(symbol));
+            }
+            lifeP += cost;
+        }
+
         return cost;
     }
 
     /**
-     * Returns the current value of the stock portfolio
+     * Returns the current value of the stock portfolio.
      * 
-     * @return currentV the current value of the stock portfolio
+     * @return currentV the current value of the stock portfolio.
      */
     public double getCurrentValue(){
         double currentV = 0;
         for (int i =0; i < stocks.size(); i++){
-            int numS = stocks.get(i).getNumShares();
+            double numS = stocks.get(i).getNumShares();
             double price = stocks.get(i).getPrice();
             currentV += (numS * price);
         }
@@ -118,9 +117,9 @@ public class Portfolio
     }
 
     /**
-     * Returns an output string formatted nicely
+     * Returns an output string formatted nicely.
      * 
-     * @return an output string formatted nicely
+     * @return an output string formatted nicely.
      */
     @Override
     public String toString()
